545f10cbe446d785b9b2c5ca35ad1172dd51f898
peng <414326615@qq.com>
2021-03-16 20:13:56 +0800

    init

---
+++ index.js
@@ -0,0 +1,152 @@
+const fs = require('fs');
+const assert = require('assert');
+
+fs.readFile('./fontFile/HanyiSentyCrayon.ttf', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+  const fontData = new Uint8Array(data);
+
+  for (let i = 0; i < 10; i++) {
+    console.log(fontData[i]);
+    console.log(getUint16([fontData[i]]));
+    console.log(getUint32([fontData[i]]));
+  }
+  
+//   const ttf = new TrueTypeFont(data);
+//   console.log(ttf.tables);
+
+});
+
+function getUint16(data) {
+    return ((data << 8) | data) >>> 0;
+}
+
+function getInt32(data) {
+    return ((data << 24) | data << 16 | data << 8 | data)
+}
+
+function getUint32(data) {
+    return getInt32(data) >>> 0
+}
+
+function BinaryReader(arrayBuffer)
+{
+    // assert(arrayBuffer instanceof ArrayBuffer);
+    this.pos = 0;
+    this.data = new Uint8Array(arrayBuffer);
+}
+
+BinaryReader.prototype = {
+    seek: function(pos) {
+        assert(pos >=0 && pos <= this.data.length);
+        var oldPos = this.pos;
+        this.pos = pos;
+        return oldPos;
+    },
+
+    tell: function() {
+        return this.pos;
+    },
+
+    getUint8: function() {//读取单字节无符号整型
+        assert(this.pos < this.data.length);
+        return this.data[this.pos++];
+    },
+
+    getUint16: function() {//读取双字节无符号整型
+        return ((this.getUint8() << 8) | this.getUint8()) >>> 0;
+    },
+
+    getUint32: function() {//读取四字节无符号整型
+       return this.getInt32() >>> 0;
+    },
+
+    getInt16: function() {//读取双字节有符号整型
+        var result = this.getUint16();
+        if (result & 0x8000) {
+            result -= (1 << 16);
+        }
+        return result;
+    }, 
+
+    getInt32: function() {//读取四字节有符号整型
+        return ((this.getUint8() << 24) | 
+                (this.getUint8() << 16) |
+                (this.getUint8() <<  8) |
+                (this.getUint8()      ));
+    }, 
+
+    getFword: function() {
+        return this.getInt16();
+    },
+
+    get2Dot14: function() {//读取定点数，00.00000000000000
+        return this.getInt16() / (1 << 14);
+    },
+
+    getFixed: function() {//读取定点数，00.00
+        return this.getInt32() / (1 << 16);
+    },
+
+    getString: function(length) {//由arraybuffer转字符串（ascii编码）
+        var result = "";
+        for(var i = 0; i < length; i++) {
+            result += String.fromCharCode(this.getUint8());
+        }
+        return result;
+    },
+
+    getDate: function() {//读取日期
+        var macTime = this.getUint32() * 0x100000000 + this.getUint32();
+        var utcTime = macTime * 1000 + Date.UTC(1904, 1, 1);
+        return new Date(utcTime);
+    }
+};
+
+function TrueTypeFont(arrayBuffer)
+{
+    this.file = new BinaryReader(arrayBuffer);
+    this.tables = this.readOffsetTables(this.file);
+    // this.readHeadTable(this.file);
+    // this.length = this.glyphCount();
+}
+
+TrueTypeFont.prototype = {
+  readOffsetTables: function(file) {
+      var tables = {};
+      this.scalarType = file.getUint32();
+      var numTables = file.getUint16();
+      this.searchRange = file.getUint16();
+      this.entrySelector = file.getUint16();
+      this.rangeShift = file.getUint16();
+
+      for( var i = 0 ; i < numTables; i++ ) {
+          var tag = file.getString(4);
+          tables[tag] = {
+              checksum: file.getUint32(),
+              offset: file.getUint32(),
+              length: file.getUint32()
+          };
+
+          if (tag !== 'head') {
+              assert(this.calculateTableChecksum(file, tables[tag].offset,
+                          tables[tag].length) === tables[tag].checksum);
+          }
+      }
+
+      return tables;
+  },
+
+  calculateTableChecksum: function(file, offset, length)
+  {
+      var old = file.seek(offset);
+      var sum = 0;
+      var nlongs = ((length + 3) / 4) | 0;
+      while( nlongs-- ) {
+          sum = (sum + file.getUint32() & 0xffffffff) >>> 0;
+      }
+
+      file.seek(old);
+      return sum;
+  }
+}
